01d53a8a305aa2cc91addad7d370d7bba146ad9b
Avinash Bodapati <54090033+avinashbo@users.noreply.github.com>
2019-08-15 14:45:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,29 +2,30 @@ provider "google" {
   project     = "terraform-demo-249805"
 }
 
-resource "google_container_cluster" "my_cluster" {
-  name     = "tf-gke-cluster"
-  location = "${var.region}"
-  remove_default_node_pool = true
-  initial_node_count = 1
+resource "google_compute_instance" "tf_test" {
+  name         = "tf-test-vm"
+  machine_type = "n1-standard-1"
+  zone         = "${var.zone}"
+
+  disk {
+    image = "debian-cloud/debian-9"
+  }
+  
+  metadata_startup_script = "echo hi > /test_terraform.txt"
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.tf-subnet.name}"
+  }
 }
 
-resource "google_container_node_pool" "my_node_pool" {
-  name       = "my-np"
-  location   = "${var.region}"
-  cluster    = "${google_container_cluster.my_cluster.name}"
-  node_count = 1
+resource "google_compute_network" "tf_network" {
+  name                    = "tf-network"
+  auto_create_subnetworks = "false"
+}
 
-  node_config {
-    machine_type = "n1-standard-1"
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
+resource "google_compute_subnetwork" "tf_subnet" {
+  name          = "tf-subnet"
+  ip_cidr_range = "10.2.0.0/24"
+  network       = "${google_compute_network.tf_network.self_link}"
+  region        = "${var.region}"
 }
