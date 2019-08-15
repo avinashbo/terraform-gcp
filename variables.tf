79614f055ba0251a79d6d7afd1e9a141fd1523ac
Avinash Bodapati <54090033+avinashbo@users.noreply.github.com>
2019-08-15 17:23:32 +0800

    Update variables.tf
    Billing account and organization Id added

--- variables.tf
+++ variables.tf
@@ -5,3 +5,11 @@ variable "region" {
 variable "zone" {
   default = "asia-southeast1-b"
 }
+
+variable "org" {
+  default = "910849363593"
+}
+
+variable "billacc" {
+  default = "01B5B2-30E970-70DACE"
+}
