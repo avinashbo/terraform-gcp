8996d31e809a10538ffde4eb36e5ced915ba12c9
Avinash Bodapati <54090033+avinashbo@users.noreply.github.com>
2019-08-28 11:17:47 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,6 @@ variable "zone" {
   default = "asia-southeast1-b"
 }
 
-variable "org" {
-  default = "910849363593"
-}
-
-variable "billacc" {
-  default = "01B5B2-30E970-70DACE"
+variable "project-d" {
+  default = "tf-demo-6611774b"
 }
