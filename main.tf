d3612ef7b8283f519d2bf8c869c72f70a5c156a7
Avinash Bodapati <54090033+avinashbo@users.noreply.github.com>
2019-08-28 11:24:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,17 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
-resource "google_project" "project" {
-  name                = "tf-demo-${random_id.suffix.hex}"
-  project_id          = "tf-demo-${random_id.suffix.hex}"
-  org_id              = "${var.org}"
-  billing_account     = "${var.billacc}"
-  auto_create_network = false
-}
+#resource "google_project" "project" {
+#  name                = "tf-demo-${random_id.suffix.hex}"
+#  project_id          = "tf-demo-${random_id.suffix.hex}"
+#  org_id              = "${var.org}"
+#  billing_account     = "${var.billacc}"
+#  auto_create_network = false
+#}
 
 resource "google_compute_instance" "tf_test_vm" {
   name         = "tf-test-vm"
-  project      = "${google_project.project.project_id}"
+  project      = "${var.project-id}"
   machine_type = "n1-standard-1"
   zone         = "${var.zone}"
 
@@ -34,7 +34,7 @@ resource "google_compute_instance" "tf_test_vm" {
 
 resource "google_compute_network" "tf_network" {
   name                    = "tf-network"
-  project                 = "${google_project.project.project_id}"
+  project                 = "${var.project-id}"
   auto_create_subnetworks = "false"
 }
 
@@ -42,20 +42,20 @@ resource "google_compute_subnetwork" "tf_subnet" {
   name          = "tf-subnet"
   ip_cidr_range = "10.2.0.0/24"
   network       = "${google_compute_network.tf_network.name}"
-  project       = "${google_project.project.project_id}"
+  project       = "${var.project-id}"
   region        = "${var.region}"
 }
 
 resource "google_storage_bucket" "tf_test_bucket" {
-  name            = "tf-bucket-${random_id.suffix.hex}"
-  location        = "${var.region}"
-  storage_class   = "REGIONAL"
-  project         = "${google_project.project.project_id}"
-}
-
-resource "google_storage_bucket" "tf_test_an_bucket" {
   name            = "tf-bucket-1-${random_id.suffix.hex}"
   location        = "${var.region}"
   storage_class   = "REGIONAL"
-  project         = "${google_project.project.project_id}"
+  project         = "${var.project-id}"
 }
+
+#resource "google_storage_bucket" "tf_test_bucket_2" {
+ # name            = "tf-bucket-2-${random_id.suffix.hex}"
+  #location        = "${var.region}"
+  #storage_class   = "REGIONAL"
+  #project         = "${var.project-id}"
+#}
